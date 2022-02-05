eb2ddf713234d16f5ad635c3a1bf076abaf905f4
Charls Catipay <charlscatipaycoc@gmail.com>
2022-02-05 16:26:30 +0800

    backup commit

--- board/login.py
+++ board/login.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.contrib import auth
@@ -11,22 +11,25 @@ class Login(TemplateView):
 	def get(self, request):
 		data = json.dumps(request.GET)
 		context = json.loads(data)
-		print(f'DATA: {context["Username"]}')
-		user = auth.authenticate(
-			username=context["Username"],
-			password=context["Password"])
-		if user is not None:
-			print(user)
-			# auth.login(request, user)
-			# if 'next' in request.POST: # to see if there is a next query coming from login_required decorator
-			# 	return redirect(request.POST.get('next')) 
-			# else:
-			# 	return redirect('index')
-		else:
-			print('User not login')
-			# return render(request,
-			# 	'login.html',
-			# 	{'error':'username or password is incorrect.'})
+		username = context['Username']
+		print(f"DATA: {username}")
+		# user = auth.authenticate(
+		# 	username=context["Username"],
+		# 	password=context["Password"])
+		# if user is not None:
+		# 	print(user)
+		# 	return redirect('board')
+		# 	# auth.login(request, user)
+		# 	# if 'next' in request.POST: # to see if there is a next query coming from login_required decorator
+		# 	# 	return redirect(request.POST.get('next')) 
+		# 	# else:
+		# 	# 	return redirect('index')
+		# else:
+		# 	print('User not login')
+		# 	return redirect('login')
+		# 	# return render(request,
+		# 	# 	'login.html',
+		# 	# 	{'error':'username or password is incorrect.'})
 		return render(
 		request, self.template_name, request.GET,
 		)
--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from board import views, login
 
 urlpatterns = [
-    path('',views.Board.as_view()),
-    path('login/',login.Login.as_view())
+    path('',views.Board.as_view(), name="board"),
+    path('login/',login.Login.as_view(), name="login")
 ]
